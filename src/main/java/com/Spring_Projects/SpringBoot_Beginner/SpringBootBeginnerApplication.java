3a72f52f66d7bbadaab6d5224c49816e1b288922
Bhanu Prakash A <avvr.bhanuprakash@gmail.com>
2026-07-27 23:09:16 +0530

    Spring initializer

---
+++ src/main/java/com/Spring_Projects/SpringBoot_Beginner/SpringBootBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Projects.SpringBoot_Beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeginnerApplication.class, args);
+	}
+
+}
