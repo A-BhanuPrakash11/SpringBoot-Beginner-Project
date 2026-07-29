d844d80fa79374cfbb40786bb590cc2da6180351
Bhanu Prakash A <avvr.bhanuprakash@gmail.com>
2026-07-29 22:23:34 +0530

    Created random user and toggle page with api call functionality

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,1 +1,54 @@
-alert("Hello from javascript ");
+//alert("Hello from javascript ");
+
+var users = [
+    {
+        "userImage": "/images/jane.png",
+        "name": "Jane",
+        "gender": "Female",
+
+    },
+    {
+        "userImage": "/images/john.png",
+        "name": "John",
+        "gender": "Male",
+
+    }
+]
+
+var id = 0;
+
+function toggleUser() {
+
+    id = (id + 1) % users.length;
+
+    var userImage = document.getElementById("userImage");
+    var userName = document.getElementById("userName");
+    var userGender = document.getElementById("userGender");
+
+    userImage.src = users[id].userImage;
+    userName.innerHTML = users[id].name;
+    userGender.innerHTML = users[id].gender;
+}
+
+function randomUser() {
+    console.log("function called successfully");
+    fetch("https://randomuser.me/api")
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (response) {
+
+            var userImage = document.getElementById("user-Image");
+            var userName = document.getElementById("user-Name");
+            var userGender = document.getElementById("user-Gender");
+
+            userImage.src = response.results[0].picture.large;
+            userName.innerHTML = response.results[0].name.first + " " + response.results[0].name.last;
+            userGender.innerHTML = response.results[0].gender;
+
+        })
+
+
+
+}
